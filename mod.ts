ce54b162622b5255dcbe39e355496f61c82cd1c6
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-10-14 03:58:21 +0900

    export modules

--- mod.ts
+++ mod.ts
@@ -1,4 +1,7 @@
 export { default as apply } from "apply";
+export * from "array";
 export * from "iter";
 export { default as pipe } from "pipe";
+export * from "pred";
 export * from "array";
+export type * from "types";
