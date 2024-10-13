930ddc35dac4d281c85d4a15f46fe784bd0381e8
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-10-14 01:10:18 +0900

    define types

---
+++ types/iter.ts
@@ -0,0 +1,30 @@
+export type Iterables<T> = T[] | IteratorObject<T> | ReadonlyArray<T>;
+
+export type Mapper<T, S> = (x: T, i: number) => S;
+export type Mappable<T> =
+  | Iterables<T>
+  | { map: <U>(f: Mapper<T, U>) => Mappable<U> };
+
+export type Predicate<T> = (x: T, i: number) => boolean;
+export type Filterable<T> =
+  | Iterables<T>
+  | { filter: (f: Predicate<T>) => Filterable<T> };
+
+export type Reducer<T, U> = (
+  previousValue: U,
+  currentValue: T,
+  currentIndex: number
+) => U;
+export type Reducible<T> =
+  | Iterables<T>
+  | { reduce: <U>(f: Reducer<T, U>, init: U) => U };
+
+export type ItersItems<T extends Iterable<unknown>[]> = T extends []
+  ? []
+  : T extends [infer Head, ...infer Tail]
+  ? Head extends Iterable<infer Item>
+    ? Tail extends Iterable<unknown>[]
+      ? [Item, ...ItersItems<Tail>]
+      : [Item]
+    : never
+  : never;
