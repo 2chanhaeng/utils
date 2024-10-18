39f4077c3352290229592329a40c36cf92ab733c
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-10-18 21:04:03 +0900

    fix toAsync

--- promise/toAsync.ts
+++ promise/toAsync.ts
@@ -1,3 +1,16 @@
-export default function toAsync<T>(promise: Iterable<Promise<T>>) {
-  return Promise.all(promise);
+/**
+ * ```haskell
+ * toAsync::a -> Promise a
+ * toAsync::[Promise a] -> Promise [a]
+ * ```
+ *
+ * Converts a value to a promise.
+ */
+export default function toAsync<T>(a: T[]): Promise<Awaited<T>[]>;
+export default function toAsync<T>(a: T): Promise<T>;
+export default function toAsync<T>(
+  a: T | T[]
+): Promise<T> | Promise<Awaited<T>[]> {
+  if (Array.isArray(a)) return Array.fromAsync(a) as Promise<Awaited<T>[]>;
+  return Promise.resolve(a);
 }
