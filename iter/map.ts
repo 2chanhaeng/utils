ddbf63dcb3e093ea70c47ee7976465ca2a81a336
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-10-18 17:26:15 +0900

    fix map

--- iter/map.ts
+++ iter/map.ts
@@ -2,28 +2,45 @@ import type { Mappable, Mapper } from "types";
 import enumerate from "./enumerate.ts";
 import { isIterable, isMappable } from "pred";
 
+/**
+ * ```haskell
+ * map::(a -> b) -> [a] -> [b]
+ * ```
+ * Map a function over an mappable object.
+ */
+
 export default function map<T, S>(
   f: Mapper<T, S>
-): (iter: Mappable<T> | Iterable<T>) => Generator<S>;
+): (iter: Iterable<T>) => Generator<S>;
 export default function map<T, S>(
   f: Mapper<T, S>,
-  iter: Mappable<T> | Iterable<T>
+  iter: Iterable<T>
 ): Generator<S>;
 export default function map<T, S>(
+  f: Mapper<T, S>
+): (iter: Mappable<T>) => Mappable<S>;
+export default function map<T, S>(
   f: Mapper<T, S>,
-  iter?: Mappable<T> | Iterable<T>
-): Generator<S> | ((iter: Mappable<T>) => Generator<S>) {
-  if (iter === undefined) return (iter: Mappable<T>) => map(f, iter);
-  return mapGen(f, iter);
+  iter: Mappable<T>
+): Mappable<S>;
+export default function map<T, S>(
+  f: Mapper<T, S>,
+  iter: Iterable<T> | Mappable<T>
+): Generator<S> | Mappable<S>;
+export default function map<T, S>(
+  f: Mapper<T, S>,
+  iter?: Iterable<T> | Mappable<T>
+):
+  | Generator<S>
+  | ((iter: Iterable<T>) => Generator<S>)
+  | Mappable<S>
+  | ((iter: Mappable<T>) => Mappable<S>) {
+  if (iter === undefined)
+    return (iter: Iterable<T> | Mappable<T>) => map(f, iter);
+  return isIterable(iter) ? mapGen(f, iter) : iter.map(f);
 }
 
-function* mapGen<T, S>(
-  f: Mapper<T, S>,
-  iter: Mappable<T> | Iterable<T>
-): Generator<S> {
-  if (isMappable(iter)) {
-    const mapped = iter.map(f);
-    if (isIterable(mapped)) yield* mapped;
-    else return mapped;
-  } else for (const [x, i] of enumerate(iter)) yield f(x, i);
+function* mapGen<T, S>(f: Mapper<T, S>, iter: Iterable<T>): Generator<S> {
+  if (isMappable<T>(iter)) yield* iter.map(f);
+  else for (const [x, i] of enumerate(iter)) yield f(x, i);
 }
