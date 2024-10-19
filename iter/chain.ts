c60ba6efee2d3946bf5c0f685484321d22f8ded4
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-10-20 01:01:53 +0900

    fix type and docs of chain

--- iter/chain.ts
+++ iter/chain.ts
@@ -1,12 +1,25 @@
+import type { Item } from "types";
+
 /**
  * ```haskell
- * flat::[[a]] -> [a]
- * ```
- * Flat multiple iterables into one.
+ * chain::[[a]] -> [a]
+ *
+ * This function takes an array of iterables and yields each element from each iterable in sequence.
+ *
+ * @template T - A tuple of iterables.
+ * @param {T} iters - An array of iterables to be chained.
+ * @returns {Generator<FlatArray<Item<T>[], 1>>} A generator that yields elements from each iterable in sequence.
+ *
+ * @example
+ * const iter1 = new Set([1, 2, 3]);
+ * const iter2 = new Set(['a', 'b', 'c']);
+ * const chained = chain([iter1, iter2]);
+ *
+ * console.log(Array.from(chained));
+ * // Output: [1, 2, 3, 'a', 'b', 'c']
  */
-export default function* chain<T extends Iterable<unknown>[]>(
+export default function* chain<T extends Iterable<unknown>>(
   iters: T
-): Generator<T extends Iterable<infer U>[] ? U : never> {
-  for (const iter of iters)
-    yield* iter as Iterable<T extends Iterable<infer U>[] ? U : never>;
+): Generator<FlatArray<Item<T>[], 1>> {
+  for (const iter of iters) yield* iter as Iterable<FlatArray<Item<T>[], 1>>;
 }
