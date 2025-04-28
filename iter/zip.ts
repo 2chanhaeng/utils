5d006857aa92a90e262b930a9c9d046fe1506a3f
ChanHaeng Lee <2chanhaeng@gmail.com>
2025-04-29 03:58:08 +0900

    fix zip

--- iter/zip.ts
+++ iter/zip.ts
@@ -6,17 +6,13 @@ import type { ItersItems } from "types";
  * ```
  * Zip iterables together.
  */
-export default function zip<T extends Iterable<unknown>[]>(
+export default function* zip<T extends Iterable<unknown>[]>(
   ...iters: T
 ): Generator<ItersItems<T>> {
-  return zipper(iters.map(Iterator.from));
-}
-
-function* zipper<T extends IteratorObject<unknown>[]>(
-  iterators: T,
-): Generator<ItersItems<T>> {
-  const results = iterators.map((iterator) => iterator.next());
-  if (results.some((result) => result.done)) return;
-  yield results.map((result) => result.value) as ItersItems<T>;
-  yield* zipper(iterators);
+  const iterators = iters.map(Iterator.from);
+  while (true) {
+    const results = iterators.map((iterator) => iterator.next());
+    if (results.some((result) => result.done)) return;
+    yield results.map((result) => result.value) as ItersItems<T>;
+  }
 }
