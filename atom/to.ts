bfc4521f7565dbf210c927ef0b495932e452e69f
ChanHaeng Lee <2chanhaeng@gmail.com>
2025-07-13 16:18:28 +0900

    add docs and lint ignore at to

--- atom/to.ts
+++ atom/to.ts
@@ -1,7 +1,14 @@
 /**
+ * ```haskell
+ * to :: (a -> b) -> a -> b
+ * ```
+ *
  * Converts a class constructor into a function that creates an instance of that class.
- * @param {Class} cls - The class constructor to convert.
+ * @param {T} cls - The class constructor to convert.
+ * @return {(...args: ConstructorParameters<T>) => InstanceType<T>} A function that takes the same parameters as the class constructor
  */
+// Use `any` cause `unknown` is not working
+// deno-lint-ignore no-explicit-any
 export default function to<T extends new (...args: any[]) => InstanceType<T>>(
   cls: T,
 ): (...args: ConstructorParameters<T>) => InstanceType<T> {
