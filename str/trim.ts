1aec759b61af76b636d6fb8d6490053caa914318
ChanHaeng Lee <2chanhaeng@gmail.com>
2025-07-18 22:28:04 +0900

    doc trim

--- str/trim.ts
+++ str/trim.ts
@@ -1,3 +1,13 @@
+/**
+ * ```haskell
+ * trim :: string -> string
+ * ```
+ *
+ * Trims whitespace from both ends of a string.
+ *
+ * @param {string} str - The string to trim.
+ * @returns {string} The trimmed string.
+ */
 export default function trim(str: string): string {
   return str.trim();
 }
